1ee3c3d0688ff01288a18392f96b24eb79ff19a9
shouryacool <84572733+shouryacool@users.noreply.github.com>
2021-10-15 09:54:30 +0530

    Add files via upload

---
+++ 0_1Strings.py
@@ -0,0 +1,14 @@
+a=34
+b="Harry"
+b="Harry's" #--->Use This If You Have Single Quote In your strings 
+b='Harry"s'
+b='''Harry"s and Harry's'''
+print(a,b)
+print(type(b))
+#  A string is a sequence of characters enclosed in quotes.
+
+# We can primarily write a string in three ways:
+
+# Single quoted strings : a = ‘harry’
+# Double quoted strings : b = “harry”
+# Triple quoted strings : c = ‘’’ harry ‘’’
